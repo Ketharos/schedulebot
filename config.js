7058224d57d578014f91c3e8d69fef4d541b13b5
Ntamari <panagkarageorgos@gmail.com>
2017-03-14 13:00:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ module.exports = {
 	// More info: https://discordapp.com/developers/docs/topics/permissions
 	delete_after_reply: {
 		enabled: true,
-		time: 3600000, // In milliseconds
+		time: 60000, // In milliseconds
 	},
 
 	// If true, it will delete any message that is not a command from the master channel.
